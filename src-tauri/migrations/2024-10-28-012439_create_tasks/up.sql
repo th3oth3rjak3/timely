c18d5e9bf489f48d9cb50242583bd51fcf29d934
th3oth3rjak3 <jake.d.hathaway@gmail.com>
2024-10-29 10:09:56 -0700

    add default value to not null columns

--- src-tauri/migrations/2024-10-28-012439_create_tasks/up.sql
+++ src-tauri/migrations/2024-10-28-012439_create_tasks/up.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    status TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'Todo',
     scheduled_start_date DATETIME,
     scheduled_complete_date DATETIME,
     actual_start_date DATETIME,
     actual_complete_date DATETIME,
     last_resumed_date DATETIME,
     estimated_duration INTEGER,
-    elapsed_duration INTEGER NOT NULL
+    elapsed_duration INTEGER NOT NULL DEFAULT 0
 );
