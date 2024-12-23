6bb9a8048149fb67ff631b21385f33174482fd1b
th3oth3rjak3 <jake.d.hathaway@gmail.com>
2024-12-23 14:01:47 -0800

    update formatting

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import "@mantine/core/styles.layer.css";
 import "@mantine/dates/styles.css";
 import "@mantine/dropzone/styles.css";
 import "@mantine/notifications/styles.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import "mantine-contextmenu/styles.css";
 import "mantine-contextmenu/styles.layer.css";
 import "mantine-datatable/styles.css";
@@ -18,8 +18,8 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={true} buttonPosition="bottom-left" />
+      <App/>
+      <ReactQueryDevtools initialIsOpen={true} buttonPosition="bottom-left"/>
     </QueryClientProvider>
   </React.StrictMode>
 );
